f9f227ae801019d7e52342b445fc0670c04f7d5c
ninodejong <nino.de.jong@nos.nl>
2022-12-15 15:47:01 +0100

    Replaced normal file open with gzdecoder

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use std::{
-    env,
-    fs::File,
-    io::{BufRead, BufReader},
-};
+use std::{env, fs::File, io::{BufReader, BufRead}};
+use flate2::read::GzDecoder;
 
 use substring::Substring;
 
@@ -17,23 +14,27 @@ fn main() {
             let dir = dir.expect("Cannot get dir entry");
             let path = dir.path();
             let path = path.to_str().expect("Cannot get path from path");
-
+            
             let file = File::open(path).expect("Unable to open file");
-            let reader = BufReader::new(file);
+            let d = GzDecoder::new(file);
+            let reader = BufReader::new(d);
 
             for line in reader.lines() {
                 let line = line.expect("Unable to read line");
                 let start = line.find(start_token);
                 let end = line.find(end_token);
-
+                
                 match (start, end) {
                     (Some(start), Some(end)) => {
+
                         let token = line.substring(start + start_token.len() + 3, end - 3);
                         println!("{}", token);
+                    },
+                    _  => {
+
                     }
-                    _ => {}
                 }
             }
-        }
+        } 
     }
 }
